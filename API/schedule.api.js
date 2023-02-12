a35e3c712101940bdc6b4865b43b305a57d1d7a7
danidufernando12 <123597071+danidufernando12@users.noreply.github.com>
2023-02-12 19:44:14 +0530

    Delete API directory

--- API/schedule.api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/schedule.controller');
-
-router.post('/add',controller.addSchedule);
-router.get('/',controller.getSchedule)
-router.get('/delete/:id',controller.deleteSchedule)
-
-module.exports=router;
