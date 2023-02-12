9f6347c35f7d155f6214ed64e8e9f8bbf34f12e6
danidufernando12 <123597071+danidufernando12@users.noreply.github.com>
2023-02-12 19:45:38 +0530

    Delete permissions directory

--- permissions/permissions.js
+++
@@ -1,9 +0,0 @@
- const jwtAuthz = require("express-jwt-authz");
-
-module.exports = checkPermissions = (permissions) => {
-  return jwtAuthz([permissions], {
-    customScopeKey: "permissions",
-    checkAllScopes: true,
-    failWithError: true
-  });
-};
